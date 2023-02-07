ffa6047b2a1c2c75b5a5356fe6a5eee26b4f24cb
laura <ldiasvitaltoledo8@gmail.com>
2023-02-07 20:08:28 -0300

    Requisitos do 20 ao 24, concluídos, corrigindo o requisito 16

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,13 +1,12 @@
-db.produtos.updateOne(
-  { nome: "Big Mac" },
-  {
-    $currentDate: {
-      ultimaModificacao: { $type: "timestamp" },
-    },
+db.produtos.updateOne({
+  nome: { $regex: /Mac/i },
+}, {
+  $currentDate: {
+    ultimaModificacao: true,
   },
-);
+});
 
-db.produtos.find(
-  { ultimaModificacao: { $exists: true } },
-  { nome: 1 },
-);
+db.produtos.find({ 
+  ultimaModificacao: { $exists: true } },
+   { _id: 0, nome: 1,
+});
