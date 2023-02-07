d4011ba33856aaa3b067c2374cc5aa7e3e12e909
laura <ldiasvitaltoledo8@gmail.com>
2023-02-07 17:44:06 -0300

    Requisitos do 8 ao 12, concluídos, corrigindo erros

--- charllenger/desafio11.js
+++
@@ -1,7 +0,0 @@
-db.produtos.find({ nome: { $nin: ["Big Mac", "McChicken"] },
-}, {
-  _id: 0,
-  nome: 1,
-  vendidos: 1,
-  curtidas: 1,
-});
--- charllenger/desafio9.js
+++
@@ -1,6 +0,0 @@
-db.produtos.find({
-  "valoresNutricionais.0.quantidade": { $lt: 500 },
-}, {
-  _id: 0,
-  nome: 1,
-});
